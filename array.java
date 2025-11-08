ebc4332d5400ba2bf9ff7bb9fabbdd35885f024d
ekansh-seth <sethekansh607@gmail.com>
2025-11-08 20:59:35 +0530

    DSA IN  JAVA

---
+++ array.java
@@ -0,0 +1,13 @@
+// array is a data structure that stores a collection 
+//of elements—typically of the same data type
+
+public class array{
+    public static void main(String[]args){
+        int [] x = {3,4,5,6,78,2};
+        //indexing
+        System.out.print(x[5]);
+        
+
+    }
+
+}
