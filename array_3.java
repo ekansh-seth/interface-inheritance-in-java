cd2cfe39050bc6cd53b094aee0b79dafdf316388
ekansh-seth <sethekansh607@gmail.com>
2025-11-08 21:01:34 +0530

    COUNTING ARRAYS IF WE NOT ABLE

---
+++ array_3.java
@@ -0,0 +1,14 @@
+///if we are not able to count your array so we find easily  
+
+ public class array_3 {
+    public static void main(String[]args){
+        int[] arr = {3,4,56,67};
+        System.out.print(arr.length);
+
+    }
+    
+}
+ //
+ // Q geve an array as a user input and doble the value and print it 
+
+ 
