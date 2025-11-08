9afb0fdaad3f9e355ae502c713181dc752dd6857
ekansh-seth <sethekansh607@gmail.com>
2025-11-08 21:05:37 +0530

    PRINTS ONLY MAX NUMBERS

---
+++ array_7.java
@@ -0,0 +1,10 @@
+public class array_7 {// print only maximum element
+    public static void main(String[] args) {
+        int[] arr = { 8,4,1 };
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max - arr[i]);
+        }
+        System.out.print(max);
+    }
+}
