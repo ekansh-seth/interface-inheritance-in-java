ed7f399a3cd912ee91d9892972573ba850bddab4
ekansh-seth <sethekansh607@gmail.com>
2025-11-08 21:03:41 +0530

    PRINT ONLY NEGATIVE NUMBERS

---
+++ array_5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class array_5 {//Give an array and print negative element 
+    private static int[] arr;
+
+    public static void main(String[]args){
+        scanner sc = new scanner();
+        System.out.println("ENTER THE ELEMENT");
+        int[]ek = new int[6];
+
+        //PRINT
+        for(int i= 0 ; i<6;i++){
+            if (arr[i]<0);
+            System.out.println(arr[i]+"");
+        }
+    }
+    
+}
