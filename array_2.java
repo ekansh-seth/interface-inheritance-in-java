6dca28d5a815cae7eb8db2f300d71415f0d75cdd
ekansh-seth <sethekansh607@gmail.com>
2025-11-08 21:00:52 +0530

    UPDATING THE ELEMENTS

---
+++ array_2.java
@@ -0,0 +1,9 @@
+public class array_2 {//updating the element 
+    public static void main (String[]args){
+        int[]x = {3,4,2,5,6,34};
+        System.out.println(x[4]); // in this condition both are print 
+        
+        x[4] = 50;
+        System.out.print(x[4]);
+
+    }}
