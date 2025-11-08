787a400a175a0f2d7cbc09543b0ebd09d8aad8dc
ekansh-seth <sethekansh607@gmail.com>
2025-11-08 21:02:48 +0530

    QUESTION ON ARRAYS
    GIVE AN ARRAYS AS A USER INPUT AND PRINTS INTO DOUBLE

---
+++ array_4.java
@@ -0,0 +1,16 @@
+ 
+ // Q geve an array as a user input and doble the value and print it 
+
+public class array_4 {
+    public static void main(String[]args){
+        scanner sc = new scanner();
+        System.out.println("enter the value");
+        int[] ekansh = new int(7);
+    }
+  // default
+    int i = 0;i<7;i ++){
+        System.out.println(ekansh[i]+"");
+    }
+
+  
+}
