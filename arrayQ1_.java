704bebf41c2d5850b0b065ebd271f5a48e693daa
ekansh-seth <sethekansh607@gmail.com>
2025-11-08 21:07:06 +0530

    GEEKS FOR GEEKS QUESTION
    ADD TWO NO AND PRINTS TARGETS ELEMENTS AND WHOLE ARRAYS

---
+++ arrayQ1_.java
@@ -0,0 +1,24 @@
+//add two first no and then we print traget on this no 
+ public class arrayQ1_ {
+    private static Object arry;
+    private static arrayQ1_ sol;
+    public int[] twosum(int[] arry,int target){
+        for(int i =0;i<arry.length;i++){
+            for(int j= i+1; j<arry.length;j++){
+                if(arry[i]+ arry[j]== target){
+
+                }
+            }
+        }
+        return arry;
+
+    }
+    public static void main(String[]args){
+        arrayQ1_ ek = new arrayQ1_();
+        int[] array = {2,7,45,34};
+        int target = 9;
+        int [] result = sol.twosum((int[]) arry,target);
+
+    }
+
+}
